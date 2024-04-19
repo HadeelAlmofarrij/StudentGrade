eda7f84e3ce267fec195e48b26037257d5b0dd0f
HadeelAlmofarrij <152024000+HadeelAlmofarrij@users.noreply.github.com>
2024-04-19 23:34:54 +0300

    Update test framework from JUnit3.8.1 to JUnit 4

--- StudentGradeTest.java
+++ StudentGradeTest.java
@@ -1,32 +1,38 @@
 package studentgrade;
 
-import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import studentgrade.StudentGrade;
 
-/* JUnit 3.8.1 */
-public class StudentGradeTest extends TestCase {
-    
-    public StudentGradeTest(String testName) {
-        super(testName);
+
+public class StudentGradeTest {
+    StudentGrade SG1=new StudentGrade();
+    public StudentGradeTest() {
     }
-
-    public void testGetGrade95() {
-        System.out.println("getGrade 95");
+    
+    @Test
+    public  void testGetGrade95() {
+      System.out.println("getGrade 95");
         int mark = 95;
         String expResult = "A";
         String result = StudentGrade.getGrade(mark);
-        assertEquals(expResult, result);
+        assertEquals(expResult, result);  
     }
-
-    public void testGetGrade85() {
+     @Test
+     public void testGetGrade85() {
         System.out.println("getGrade 85");
         int mark = 85;
         String expResult = "B";
         String result = StudentGrade.getGrade(mark);
         assertEquals(expResult, result);
     }
-    
+     @Test
     public void testGetGrade75() {
-        System.out.println("getGrade 85");
+        System.out.println("getGrade 75");
         int mark = 75;
         String expResult = "F";
         String result = StudentGrade.getGrade(mark);
